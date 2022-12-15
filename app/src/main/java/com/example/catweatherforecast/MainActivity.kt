7c56c447d4e3cecdcdeacd1d172adc5cab1e734a
CatAvadani <catalinaavadani@gmail.com>
2022-12-15 21:55:38 +0100

    Dependencies setup

--- app/src/main/java/com/example/catweatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/catweatherforecast/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Catlina")
+                    MyApp()
                 }
             }
         }
@@ -30,14 +30,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    Text(text = "Hello there")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CatWeatherForecastTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
