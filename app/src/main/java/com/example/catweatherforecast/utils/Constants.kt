9227f30f46b75ec6e51401c50131e7f62cd6471d
CatAvadani <catalinaavadani@gmail.com>
2022-12-16 19:40:44 +0100

    Update Constants

--- app/src/main/java/com/example/catweatherforecast/utils/Constants.kt
+++ app/src/main/java/com/example/catweatherforecast/utils/Constants.kt
@@ -2,6 +2,5 @@ package com.example.catweatherforecast.utils
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org/"
-  //  const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
 
 }
