fd367a8565ad191ab3855c934b9138d2ab916e74
CatAvadani <97651171+CatAvadani@users.noreply.github.com>
2022-12-16 19:38:18 +0100

    Delete Constants.kt

--- app/src/main/java/com/example/catweatherforecast/utils/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.catweatherforecast.utils
-
-object Constants {
-    const val BASE_URL = "https://api.openweathermap.org/"
-  //  const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
-
-}
