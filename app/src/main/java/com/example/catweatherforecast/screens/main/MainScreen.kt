3f094d9316bbbd7b127eb8cc7cb770b7e05c6f71
CatAvadani <catalinaavadani@gmail.com>
2022-12-17 13:18:39 +0100

    Adding the Url for the image of weather conditions

--- app/src/main/java/com/example/catweatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/catweatherforecast/screens/main/MainScreen.kt
@@ -1,55 +1,115 @@
 package com.example.catweatherforecast.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.catweatherforecast.data.DataOrException
 import com.example.catweatherforecast.model.Weather
 import com.example.catweatherforecast.screens.main.MainViewModel
+import com.example.catweatherforecast.widgets.WeatherAppBar
 
 @Composable
 fun MainScreen(navController: NavController, mainViewModel: MainViewModel = hiltViewModel()) {
-    ShowData(mainViewModel)
-}
-
-@Composable
-fun ShowData(mainViewModel: MainViewModel) {
     val weatherData =
         produceState<DataOrException<Weather, Boolean, Exception>>(
             initialValue = DataOrException(
                 loading = true
             )) {
-                value = mainViewModel.getWeatherData(city = "Seattle")
-            }.value
+            value = mainViewModel.getWeatherData(city = "Bucharest")
+        }.value
     if (weatherData.loading == true) {
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
 
-      Surface(modifier = Modifier.fillMaxSize(),color = Color(0xE1FF6538)) {
-          Column(verticalArrangement = Arrangement.Center,
-              horizontalAlignment = Alignment.CenterHorizontally
-          ) {
-              Text(text = "Main Screen ${weatherData.data.toString()}")
-          }
-      }
+        Surface(modifier = Modifier.fillMaxSize(),color = Color(0xE1FF6538)) {
+            Column(verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                MainScaffold(weather = weatherData.data!!, navController)
+            }
+        }
+    }
+}
 
-}}
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+    Scaffold(topBar = {
+        WeatherAppBar(title = weather.city?.name.toString() +", ${weather.city?.country}",
+            navController = navController, elevation = 5.dp) {
+            Log.d("Tag", "MainScaffold: Button Clicked")
+        }
+    }, backgroundColor = Color.White) {
+        MainContent(data = weather)
+    }
 
+}
+
+@Composable
+fun MainContent(data: Weather) {
+    val imageUrl = "https://openweathermap.org/img/wn/${data.list?.get(0)?.weather?.get(0)?.icon}.png"
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Nov 29",
+            style = MaterialTheme.typography.caption,
+            fontWeight = FontWeight.SemiBold,
+            color = MaterialTheme.colors.onSecondary,
+            modifier = Modifier.padding(6.dp)
+        )
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(text = "56", style = MaterialTheme.typography.h4, fontWeight = FontWeight.ExtraBold)
+                Text(text = "Snow", fontStyle = FontStyle.Italic)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    
+    Image(painter = rememberAsyncImagePainter(
+        model = imageUrl),
+        contentDescription = "Weather Icon",
+        modifier = Modifier.size(80.dp)
+    )
+
+}
 
 @Preview
 @Composable
 fun MainScreenPreview() {
-    MainScreen(navController = rememberNavController())
+    MainScaffold(weather = Weather(), navController = rememberNavController())
 }
---
+++ app/src/main/java/com/example/catweatherforecast/widgets/WeatherAppBar.kt
@@ -0,0 +1,84 @@
+package com.example.catweatherforecast.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun WeatherAppBar(
+    title: String = "Title",
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {}
+) {
+    TopAppBar(
+        modifier = Modifier,
+        //  backgroundColor = Color(0xFF64D0FC),
+        backgroundColor = Color.Transparent,
+        title = {
+            Text(
+            text = title,
+            color = Color.Black,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+                fontSize = 15.sp
+            ) },
+        actions = {
+            if (isMainScreen) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search Icon",
+                        tint = Color.Black
+                    )
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Rounded.MoreVert,
+                        contentDescription = "More Icon",
+                        tint = Color.Black
+
+                    )
+                }
+            } else {
+                Box {}
+            }
+        },
+        navigationIcon = {
+                         if (icon != null) {
+                             Icon(
+                                 imageVector = icon,
+                                 contentDescription = null,
+                                 tint = MaterialTheme.colors.onSecondary,
+                                 modifier = Modifier.clickable {
+                                     onButtonClicked.invoke()
+                                 }
+                             )
+                         }
+        },
+        elevation = elevation
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeatherAppBarPreview() {
+    WeatherAppBar(navController = rememberNavController())
+}
