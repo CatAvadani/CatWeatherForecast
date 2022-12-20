c2e9b871e2e7837ea288024440442a49605e7480
CatAvadani <catalinaavadani@gmail.com>
2022-12-20 16:13:31 +0100

    Update the Search Screen

--- app/src/main/java/com/example/catweatherforecast/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/catweatherforecast/screens/search/SearchScreen.kt
@@ -1,68 +1,125 @@
 package com.example.catweatherforecast.screens.search
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.catweatherforecast.widgets.WeatherAppBar
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SearchScreen(navController: NavController) {
     Scaffold(
         topBar = {
-            TopAppBar(modifier = Modifier.padding(2.dp),
-                backgroundColor = Color(0xFFF7F29E)
-            ) {
-                Row(horizontalArrangement = Arrangement.Start
+            WeatherAppBar(
+               navController = navController,
+                title = "Search",
+                icon = Icons.Default.ArrowBack,
+                isMainScreen = false,
                 ) {
-                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription =" Back Arrow" )
-                    Text(
-                        text = "Search",
-                        modifier = Modifier.padding(start = 140.dp),
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 20.sp,
-                        color = Color.Black
-                    )
-                }
+               navController.popBackStack()
             }
-        }
+        },
+        backgroundColor = Color.White
     ) {
-        Surface(
-            modifier = Modifier
-                .padding(4.dp)
-                .fillMaxWidth()
-                .height(35.dp)
-            ,
-            shape = RoundedCornerShape(25),
-            border = BorderStroke(1.dp, color = Color.Black)
-        ) {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.Start
-            ) {
-                Text(
-                    text = "Search Screen",
-                    modifier = Modifier.padding(4.dp),
-                    style = TextStyle(fontSize = 18.sp),
-                    color = Color.Black)
-            }
-        }
+
+          Column(
+              verticalArrangement = Arrangement.Center,
+              horizontalAlignment = Alignment.CenterHorizontally
+          )
+              {
+              SearchBar(modifier = Modifier
+                  .fillMaxWidth()
+                  .padding(16.dp)
+                  .align(Alignment.CenterHorizontally)){
+                  Log.d("TAG", "Search Screen: $it")
+              }
+          }
+      }
     }
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    onSearch: (String) -> Unit = {}
+) {
+    val searchQueryState = rememberSaveable { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(searchQueryState.value){
+           searchQueryState.value.trim().isNotEmpty()
+    }
+    Column {
+        CommonTextField(
+            valueState = searchQueryState,
+            placeHolder = "Seattle",
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onSearch(searchQueryState.value.trim())
+                searchQueryState.value = ""
+                keyboardController?.hide()
+            }
+        )
+    }
+}
+
+@Composable
+fun CommonTextField(valueState: MutableState<String>,
+                    placeHolder: String,
+                    keyboardType: KeyboardType = KeyboardType.Text,
+                    imeAction: ImeAction = ImeAction.Next,
+                    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = {
+        valueState.value = it
+    },
+        label = { Text(text = placeHolder, color = Color.Black)},
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+        keyboardActions = onAction,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Blue,
+            unfocusedBorderColor = Color.LightGray,
+            backgroundColor = Color.White,
+            cursorColor = Color.Black,
+            textColor = Color.Black
+        ),
+        shape = RoundedCornerShape(15.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 10.dp, end = 10.dp)
+
+    )
 
 }
 
